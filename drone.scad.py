3d94fb603e3616154fb4a5ae5f985178e5d3d28f
Thomas Draper <tom@betterservers.com>
2020-02-17 20:04:14 -0800

    Add notch to cross bar and resolution option.

--- drone.scad.py
+++ drone.scad.py
@@ -9,6 +9,7 @@ height = 20
 end_diameter = 50
 end_hole = 10
 gap = .4  # .4mm is a typical tight gap for 3D printed components
+fn = 256 # Sides of circle
 
 edge_circle = sd.circle(d=end_diameter)
 edge_hole = sd.circle(d=end_hole)
@@ -20,4 +21,8 @@ cross_beam -= sd.translate([-length/2, 0])(edge_hole)
 
 cross_beam = sd.linear_extrude(height)(cross_beam)
 
-print(sd.scad_render(cross_beam))
+notch = sd.cube([width+2*gap, length, height], center=True)
+notch = sd.translate([0, 0, height-gap/2])(notch)
+
+cross_beam -= notch
+print(sd.scad_render(cross_beam, file_header='$fn=%d;'%fn))
